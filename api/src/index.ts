02365b8205bf0d6ac1a4a6ef35dc32bb83715a88
Jogeshwar Singh <singhjogeshwar007@gmail.com>
2024-06-22 12:46:28 +0530

    feat: add cors and get share

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,9 +1,15 @@
 import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
+app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
+app.get("/share", (req, res) => {
+  res.json({
+    share: process.env.SHARE,
+  });
 });
 
 app.listen(3000, () => {
